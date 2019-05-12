1b200c411fad8b83a4af4a55433a0799429ca193
bmzhu2 <brian.m.zhu@gmail.com>
2019-05-11 22:14:29 -0700

    Create user auth process

---
+++ validation/login.js
@@ -0,0 +1,23 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function(data) {
+  errors: {};
+
+  data.user = validText(data.usernane) ? data.username : ' '
+  data.password = validText(data.password) ? data.password : ' '
+
+  if(Validator.isEmpty(data.username)) {
+    errors.username = "Username field is required";
+  }
+
+  if(Validator.isEmpty(data.password)) {
+    errors.username = "Password field is required";
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  }
+
+}
