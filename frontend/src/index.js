1272ef223bbf22046ae156d5144676b1edad0c8c
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-12 15:52:57 -0700

    Update index and components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import Login from './components/forms/login';
-import Register from './components/forms/register';
-
 
 ReactDOM.render(
-  <Register />,
+  <App />,
   document.getElementById('root')
 );
