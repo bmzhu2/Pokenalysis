481c0bc5fda9ad4e53a6cb0358b3312d2a08ed4b
Samuel Inglese <senglese96@gmail.com>
2019-05-12 15:39:22 -0700

    create index redux cycle

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import configureStore from './store/store';
+
+import jwt_decode from 'jwt-decode';
+
+import {setAuthToken} from './util/session_api_util'
+
+import {logout} from './actions/session_actions.js'
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  let store;
+
+  if(localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+
+    const decodedUser = jwt_decode(localStorage.jwtToken);
+
+    const preloadedState = { session: { isAuthenticated: true, user: decodedUser}};
+
+    store = configureStore(preloadedState);
+
+    const currentTime = Date.now() / 1000;
+
+    if(decodedUser.exp < currentTime) {
+      store.dispatch(logout());
+      window.location.href = '/login'
+    }
+  } else{
+    store = configureStore({});
+  }
+})
