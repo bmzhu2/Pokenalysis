1568a2bbd03c2bffb3afdb8caf01b33b4fa38db8
Samuel Inglese <senglese96@gmail.com>
2019-05-12 17:25:30 -0700

    Clean stuff up

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -5,7 +5,6 @@ import jwt_decode from 'jwt-decode';
 
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
-export const RECEIVE_USER_SIGNIN = 'RECEIVE_USER_SIGNIN'
 export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS'
 
 export const receiveCurrentUser = currentUser => ({
--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_USER_LOGOUT, RECEIVE_CURRENT_USER, RECEIVE_USER_SIGNIN } from '../actions/session_actions'
+import { RECEIVE_USER_LOGOUT, RECEIVE_CURRENT_USER} from '../actions/session_actions'
 
 const initialState = {
     isAuthenticated: false,
