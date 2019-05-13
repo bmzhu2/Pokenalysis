678a63b40e171cb478f0c72b7f9eae1a849402fe
Samuel Inglese <senglese96@gmail.com>
2019-05-12 21:18:29 -0700

    Create team actions and teams and entities reducer

---
+++ frontend/src/actions/team_actions.js
@@ -0,0 +1,50 @@
+import * as APIUtil from '../util/team_api_util'
+
+export const RECEIVE_TEAMS = 'RECEIVE_TEAMS';
+export const RECEIVE_TEAM = 'RECEIVE_TEAM';
+export const REMOVE_TEAM = 'REMOVE_TEAM';
+
+const receiveTeams = (teams) => ({
+    type: RECEIVE_TEAMS,
+    teams
+})
+
+const receiveTeam = team => ({
+    type: RECEIVE_TEAM,
+    team
+})
+
+const removeTeam = teamId => ({
+    type: REMOVE_TEAM,
+    teamId
+})
+
+export const fetchTeams = () => dispatch => {
+    APIUtil.fetchTeams()
+        .then(teams => dispatch(receiveTeams(teams)))
+}
+
+export const fetchTeam = (teamId) => dispatch => {
+    APIUtil.fetchTeam(teamId)
+        .then(team => dispatch(receiveTeam(team)))
+}
+
+export const fetchUserTeams = userId => dispatch => {
+    APIUtil.fetchUserTeams(userId)
+        .then(teams  => dispatch(receiveTeams(teams)))
+}
+
+export const createTeam =  team => dispatch => {
+    APIUtil.createTeam(team)
+        .then(team => dispatch(receiveTeam(team)))
+}
+
+export const updateTeam = team => dispatch => {
+    APIUtil.updateTeam(team)
+        .then(team => dispatch(receiveTeam(team)))
+}
+
+export const deleteTeam = teamId => dispatch => {
+    APIUtil.deleteTeam(teamId)
+        .then(teamId => dispatch(removeTeam(teamId)))
+}
---
+++ frontend/src/reducers/teams_reducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_TEAM, RECEIVE_TEAMS, REMOVE_TEAM } from '../actions/team_actions'
+
+const teamsReducer = (state={}, action) => {
+    Object.freeze(state);
+    switch(action.type){
+        case RECEIVE_TEAM:
+            return Object.assign({}, state, {[action.team.id]: action.team})
+        case RECEIVE_TEAMS:
+            return Object.assign({}, state, action.teams)
+        case REMOVE_TEAM:
+            let newState = state;
+            delete newState[action.teamId];
+            return newState;
+        default:
+            return state
+    }
+}
+
+export default teamsReducer
