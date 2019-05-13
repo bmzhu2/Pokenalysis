6dc7451b43f95221e807497b9e367208872e3612
bmzhu2 <brian.m.zhu@gmail.com>
2019-05-12 22:11:46 -0700

    Adds nature util file with an object of all natures and their effects

---
+++ frontend/src/util/nature_util.js
@@ -0,0 +1,32 @@
+export const natures = {
+  lonely: "+Atk, -Def",
+  adamant: "+Atk, -Sp.Atk",
+  naughty: "+Atk, -Sp.Def",
+  brave: "+Atk, -Spd",
+
+  bold: "+Def, -Atk",
+  impish: "+Def, -Sp.Atk",
+  lax: "+Def, -Sp.Def",
+  relaxed: "+Def, -Spd",
+  
+  modest: "+Sp.Atk, -Atk",
+  mild: "+Sp.Atk, -Def",
+  rash: "+Sp.Atk, -Sp.Def",
+  quiet: "+Sp.Atk, -Spd",
+  
+  calm: "+Sp.Def, -Atk",
+  gentle: "+Spd.Def, -Def",
+  careful: "+Sp.Def, -Sp.Atk",
+  sassy: "+Sp.Def, -Spd",
+  
+  timid: "+Spd, -Atk",
+  hasty: "+Spd, -Def",
+  jolly: "+Spd, -Sp.Atk",
+  naive: "+Spd, -Sp.Def",
+  
+  bashful: "No effect",
+  docile: "No effect",
+  hardy: "No effect",
+  quirky: "No effect",
+  serious: "No effect"
+}
