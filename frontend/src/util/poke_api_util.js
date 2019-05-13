140c77a736f30221f35f99ec3db83c11974acaeb
bmzhu2 <brian.m.zhu@gmail.com>
2019-05-12 21:07:39 -0700

    Creates util file for axios calls to pokeapi

---
+++ frontend/src/util/poke_api_util.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const spriteAddress = id => {
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+
+}
+
+export const fetchPokemon = id => {
+  return axios.get('https://pokeapi.co/api/v2/pokemon/' + id)
+}
+
+export const fetchAllPokemon = () => {
+  return axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=964')
+}
+
+export const fetchMove = name => {
+  return axios.get('https://pokeapi.co/api/v2/move/' + name)
+}
+
+export const fetchAbility = name => {
+  return axios.get('https://pokeapi.co/api/v2/ability/' + name)
+}
