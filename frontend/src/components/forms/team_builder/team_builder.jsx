623c61b719482a4266dc7d847e6d9ab34b02488b
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-13 10:00:19 -0700

    Create team_builder container

---
+++ frontend/src/components/forms/team_builder/team_builder.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class TeamBuilder extends React.Component {
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+        return(
+            <div>
+                This is Team builder
+            </div>
+        )
+    }
+}
+
+export default TeamBuilder;
