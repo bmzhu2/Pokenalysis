cac9ca508f5d05a7685e99f1bf3980f47b4a8fa9
Samuel Inglese <senglese96@gmail.com>
2019-05-13 11:09:03 -0700

    create like reducer and add it to entities reducer

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import teamsReducer from './teams_reducer'
 import pokemonReducer from './pokemon_reducer'
 import { combineReducers } from 'redux'
 import commentsReducer from './comments_reducer'
+import likesReducer from './likes_reducer';
 
 const entitiesReducer = combineReducers({
     teams: teamsReducer,
     pokemon: pokemonReducer,
-    comments: commentsReducer
+    comments: commentsReducer,
+    likes: likesReducer
 })
 
 export default entitiesReducer
