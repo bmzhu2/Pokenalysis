d53a9110fb75d2b86cb8c05adba21a762aec52ed
Samuel Inglese <senglese96@gmail.com>
2019-05-14 14:01:28 -0700

    heroku stuff

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const likes = require("./routes/api/likes");
 const comments = require("./routes/api/comments");
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 mongoose
 .connect(db, { useNewUrlParser: true })
