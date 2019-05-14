9bcf608a0d06693fd494e4e092ce9f2edc2d5ce0
Samuel Inglese <senglese96@gmail.com>
2019-05-13 22:20:08 -0700

    create more seeds

--- seed.js
+++ seed.js
@@ -49,6 +49,16 @@ const dataUsers = [
                 'username': 'Zinnia',
                 'password': 'GetSalaMinced',
                 'id': '41224d776a326fb40f000000'
+            },
+            {
+                'username': 'Sabrina',
+                'password': 'GetPsyched',
+                'id': '41224d776a326fb40f000010'
+            },
+            {
+                'username': 'Diantha',
+                'password': 'XYted',
+                'id': '41224d776a326fb40f000020'
             }
         ]
     }
@@ -119,6 +129,117 @@ const dataTeams = [
                 ],
                 'user': '41224d776a326fb40f000001',
                 'id': '41224d776a326fb40f000002'
+            },
+            {
+                'name': "mind crush",
+                'pokemon':[
+                    {
+                        'pokeId': 124,
+                        'move1': 'psychic',
+                        'move2': 'focus-blast',
+                        'move3': 'blizzard',
+                        'move4': 'perish-song',
+                        'ability': 'forewarn'
+                    },
+                    {
+                        'pokeId': 202,
+                        'move1': 'counter',
+                        'move2': 'destiny-bond',
+                        'move3': 'mirror-coat',
+                        'move4': 'encore',
+                        'ability': 'shadow-tag'
+                    },
+                    {
+                        'pokeId': 475,
+                        'move1': 'stone-edge',
+                        'move2': 'psycho-cut',
+                        'move3': 'night-slash',
+                        'move4': 'close-combat',
+                        'ability': 'steadfast'
+                    },
+                    {
+                        'pokeId': 065,
+                        'move1': 'gravity',
+                        'move2': 'psychic',
+                        'move3': 'focus-blast',
+                        'move4': 'energy-ball',
+                        'ability': 'synchronize'
+                    },
+                    {
+                        'pokeId': 196,
+                        'move1': 'calm-mind',
+                        'move2': 'psychic',
+                        'move3': 'shadow-ball',
+                        'move4': 'baton-pass',
+                        'ability': 'synchronize'
+                    },
+                    {
+                        'pokeId': 122,
+                        'move1': 'reflect',
+                        'move2': 'psychic',
+                        'move3': 'thunder',
+                        'move4': 'skill-swap',
+                        'ability': 'filter'
+                    }
+                ],
+                'user': '41224d776a326fb40f000010',
+                'id': '41224d776a326fb40f000006'
+            },
+            {
+                'name': 'Team Diantha',
+                'pokemon': [
+                    {
+                        'pokeId': 701,
+                        'move1': 'swords-dance',
+                        'move2': 'flying-press',
+                        'move3': 'x-scissor',
+                        'move4': 'poison-jab',
+                        'ability': 'limber'
+                    },
+                    {
+                        'pokeId': 697,
+                        'move1': 'head-smash',
+                        'move2': 'earthquake',
+                        'move3': 'dragon-claw',
+                        'move4': 'crunch',
+                        'ability': 'strong-jaw'
+                    },
+                    {
+                        'pokeId': 699,
+                        'move1': 'thunder',
+                        'move2': 'blizzard',
+                        'move3': 'light-screen',
+                        'move4': 'reflect',
+                        'ability': 'refrigerate'
+                    },
+                    {
+                        'pokeId': 706,
+                        'move1': 'dragon-pulse',
+                        'move2': 'muddy-water',
+                        'move3': 'focus-blast',
+                        'move4': 'fire-blast',
+                        'ability': 'sap-sipper'
+                    },
+                    {
+                        'pokeId': 711,
+                        'move1': 'trick-or-treat',
+                        'move2': 'phantom-force',
+                        'move3': 'shadow-sneak',
+                        'move4': 'seed-bomb',
+                        'ability': 'pickup'
+                    },
+                    {
+                        'pokeId': 10051,
+                        'move1': 'moonblast',
+                        'move2': 'psychic',
+                        'move3': 'shadow-ball',
+                        'move4': 'thunderbolt',
+                        'item': 'gardevoirite',
+                        'ability': 'pixilate'
+                    }
+                ],
+                'user': '41224d776a326fb40f000020',
+                'id': '41224d776a326fb40f000021'
             }
         ]
     }
