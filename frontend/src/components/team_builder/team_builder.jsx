edac837c38df23646f4d496ae6380118cdf0fb01
PNowakowski <p.nowakowskimusic@gmail.com>
2019-05-14 10:10:38 -0700

    Working lazy load on teambuilder

--- frontend/src/components/team_builder/team_builder.jsx
+++ frontend/src/components/team_builder/team_builder.jsx
@@ -68,26 +68,6 @@ class TeamBuilder extends React.Component {
             });
         }); 
         
-        const targets = document.querySelectorAll('img');
-
-        const lazyLoad = target => {
-            const io = new IntersectionObserver((entries, observer) => {
-                entries.forEach(entry => {
-                    console.log(entry);
-
-                    if (entry.isIntersecting) {
-                        const img = entry.target;
-                        const src = img.getAttribute('data-lazy');
-
-                        img.setAttribute('src', src);
-
-                        observer.disconnect();
-                    }
-                });
-            });
-            io.observe(target);
-        };
-        targets.forEach(target => lazyLoad(target));
     }
 
     handleScroll() {
