f4447b052ff20506912932db05b7088dd03edcac
bmzhu2 <brian.m.zhu@gmail.com>
2019-05-15 09:39:30 -0700

    Implements demo user

--- frontend/src/components/forms/login.jsx
+++ frontend/src/components/forms/login.jsx
@@ -13,6 +13,7 @@ class Login extends React.Component {
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.renderErrors = this.renderErrors.bind(this);
+        this.demoLogin = this.demoLogin.bind(this);
     }
 
     update(field) {
@@ -46,34 +47,65 @@ class Login extends React.Component {
         )
     }
 
+    demoLogin() {
+        const user = {username: "Zinnia", password: "GetSalaMinced"}
+        var login = this.props.login;
+        login = this.props.login.bind(this);
+        login(user)
+            .then(data => {
+                if (data === undefined) {
+                    this.props.history.push("/");
+                } else {
+                    console.log("error logging in");
+                }
+            });  
+    }
+
 
     render(){
 
-        return(
-            <div className="content-container">
-                <div className="form">
-                    <div className="splash-logo"></div>
-                    <h1>Login to Pokenalysis</h1>
-                    <form onSubmit={this.handleSubmit}>
-                        <div className="form-row">
-                            <label htmlFor="username-login">Username</label>
-                            <input id="username-login" type="text" onChange={this.update("username")} />
-                        </div>
-                        <div className="form-row">
-                            <label htmlFor="password-login">Password</label>
-                            <input id="password-login" type="password" onChange={this.update("password")} />
-                        </div>
-                        <div className="form-row">
-                            <input type="submit" className="submit" value="Login" />                    
-                        </div>
-                        {this.renderErrors()}
-                        <div className="form-row">
-                            <Link to={'/register'}>Need to create an account?</Link>
-                        </div>
-                    </form>
+        return (
+          <div className="content-container">
+            <div className="form">
+              <div className="splash-logo" />
+              <h1>Login to Pokenalysis</h1>
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-row">
+                  <label htmlFor="username-login">Username</label>
+                  <input
+                    id="username-login"
+                    type="text"
+                    onChange={this.update("username")}
+                  />
                 </div>
+                <div className="form-row">
+                  <label htmlFor="password-login">Password</label>
+                  <input
+                    id="password-login"
+                    type="password"
+                    onChange={this.update("password")}
+                  />
+                </div>
+                <div className="form-row">
+                  <input
+                    type="submit"
+                    className="submit"
+                    value="Login"
+                  />
+                </div>
+                {this.renderErrors()}
+              </form>
+              <div className="form-row">
+                <button onClick={this.demoLogin}>
+                  <p>Demo User</p>
+                </button>
+              </div>
+              <div className="form-row">
+                <Link to={"/register"}>Need to create an account?</Link>
+              </div>
             </div>
-        )
+          </div>
+        );
     }
 
 }
