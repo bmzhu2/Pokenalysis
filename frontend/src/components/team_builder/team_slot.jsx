e580857cf27d317040033a5f8a7a00afec072b89
PNowakowski <p.nowakowskimusic@gmail.com>
2019-06-07 12:54:29 -1000

    Reposition pokemon sprite and name in slots

--- frontend/src/components/team_builder/team_slot.jsx
+++ frontend/src/components/team_builder/team_slot.jsx
@@ -47,13 +47,14 @@ class TeamSlot extends React.Component {
         const { id, pokeId, name, sprite, removeFromTeam } = this.props;
         const { isOver, canDrop, connectDropTarget } = this.props;
         return connectDropTarget(
-            <li onClick={this.props.setAttrId} className={this.state.isHovered ? "team-slot hovered-slot" : "team-slot"}>
-
-                <p className={sprite ? "none" : "plus"}>+</p>
-                <img className={sprite ? "team-slot-sprite" : ""} src={sprite} alt=""/>
+            <li onClick={this.props.setAttrId} className="team-slot-outer-container">
+                <p onClick={() => removeFromTeam(id)} className={sprite ? "x" : "none"}>x</p> 
+                <div className={this.state.isHovered ? "team-slot hovered-slot" : "team-slot"}>
+                    <p className={sprite ? "none" : "plus"}>+</p>
+                    <img className={sprite ? "team-slot-sprite" : ""} src={sprite} alt=""/>
+                </div>
                 <div className="team-slot-header"> 
                     <h3 className="team-slot-name">{name}</h3>
-                    <p onClick={() => removeFromTeam(id)} className={sprite ? "x" : "none"}>x</p> 
                 </div>
             </li>
         )
