0f75a1c77a82f83b3ff3c4bb62a3ced5fba13b29
PNowakowski <p.nowakowskimusic@gmail.com>
2019-06-08 13:57:52 -1000

    Shrink team name and save button on scroll

--- frontend/src/components/team_builder/team_builder.jsx
+++ frontend/src/components/team_builder/team_builder.jsx
@@ -268,8 +268,8 @@ class TeamBuilder extends React.Component {
             <div className="team-builder-container" >
                     <div className={scrollY ? "sticky-container" : "sticky-container no-border"}>
                         <div className="name-submit-container">
-                            <input className="team-name" onChange={this.updateTeamName()} type="text" placeholder={"New Team"}/>
-                            <input className="submit-team" onClick={this.saveTeam} type="submit" value="Save"/>
+                            <input className={scrollY ? "team-name minimized-name" : "team-name"} onChange={this.updateTeamName()} type="text" placeholder={"New Team"}/>
+                            <input className={scrollY ? "submit-team minimized-submit" : "submit-team"} onClick={this.saveTeam} type="submit" value="Save"/>
                         </div>
                         <div className="team-slots-container-container">
                             <ul className="team-slots-container"> 
